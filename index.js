c28be20a64c0b2401dafbffd96123cacabbf624a
Fengda HUANG <h@phodal.com>
2014-11-18 23:30:26 +0800

    Add the github support

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var Github = require('./server/github_support');
 
-var gs = new Github('phodal');
-gs.get('phodal');
+var gs = new Github();
+gs.get('phodal', function (result) {
+    'use strict';
+    var response = gs.on(result)
+        .add_avatar()
+        .add_connected_users()
+        .add_name()
+        .add_repositories()
+        .add_languages()
+        .add_events()
+        .add_day_hours()
+        .add_similar_users()
+        .build();
+
+    console.log(response);
+});
--- server/github_support.js
+++ server/github_support.js
@@ -1,16 +1,71 @@
 var request = require('request');
+var _       = require('underscore');
 
-function github_support(name){
+function github_support() {
     'use strict';
-    this.name = name;
+    this.result    = [];
+    this.response  = [];
 }
 
-github_support.prototype.get = function() {
-    var uri = 'https://osrc.dfm.io/' + this.name + '.json';
+github_support.prototype.get = function(name, callback) {
+    //var source = 'https://osrc.dfm.io/';
+    var source = 'http://0.0.0.0:10000/';
+    var uri = source + name + '.json';
     request(uri, function(error, response, body) {
-            console.log(body);
-            console.log(response.headers['content-type']);
-        })
+        this.result = body;
+        callback(body);
+    });
+};
+
+github_support.prototype.on = function (result) {
+    this.result = result;
+    if(_.isEmpty(result)) {
+        throw new Error("no this id");
+    }
+    return this;
+};
+
+github_support.prototype.add_avatar = function () {
+    _.extend(this.response, {"avatar" : JSON.parse(this.result).avatar});
+    return this;
+};
+
+github_support.prototype.add_connected_users = function () {
+    _.extend(this.response, {"connected_users": JSON.parse(this.result).connected_users});
+    return this;
+};
+
+github_support.prototype.add_name = function () {
+    _.extend(this.response, {"name": JSON.parse(this.result).name});
+    return this;
+};
+
+github_support.prototype.add_repositories = function () {
+    _.extend(this.response, {"repositories": JSON.parse(this.result).repositories});
+    return this;
+};
+
+github_support.prototype.add_events = function () {
+    _.extend(this.response, {"events": JSON.parse(this.result).usage.events});
+    return this;
+};
+
+github_support.prototype.add_day_hours = function () {
+    _.extend(this.response, {"day_hours": JSON.parse(this.result).usage.day});
+    return this;
+};
+
+github_support.prototype.add_similar_users = function () {
+    _.extend(this.response, {"similar_users": JSON.parse(this.result).usage.similar_users});
+    return this;
+}
+github_support.prototype.add_languages = function () {
+    _.extend(this.response, {"languages": JSON.parse(this.result).usage.languages});
+    return this;
+};
+
+github_support.prototype.build = function () {
+    return this.response;
 };
 
 module.exports = github_support;
