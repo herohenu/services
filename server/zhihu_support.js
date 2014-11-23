c71f7a9dc83dc9a5bc412d396a646d49684c85b5
Fengda HUANG <h@phodal.com>
2014-11-23 15:03:43 +0800

    Add the zhihu support

--- server/zhihu_support.js
+++ server/zhihu_support.js
@@ -4,24 +4,24 @@ var fs       = require('fs');
 
 var jquery = fs.readFileSync("./lib/jquery-2.1.1.min.js", "utf-8");
 
-function csdn_support() {
+function zhihu_support() {
     'use strict';
     return;
 }
 
-csdn_support.prototype.add_user_agree = function ($, result) {
+zhihu_support.prototype.add_user_agree = function ($, result) {
     'use strict';
     var agree_number = $(".zm-profile-header-user-agree").find('strong').text();
     result.push({agree: agree_number});
 };
 
-csdn_support.prototype.add_user_thanks = function ($, result) {
+zhihu_support.prototype.add_user_thanks = function ($, result) {
     'use strict';
     var thanks_number = $(".zm-profile-header-user-agree").find('strong').text();
     result.push({thanks: thanks_number});
 };
 
-csdn_support.prototype.add_good_zone = function ($, result) {
+zhihu_support.prototype.add_good_zone = function ($, result) {
     'use strict';
     var skilledTopics = [];
     $(".zg-gray-darker").each(function() {
@@ -30,7 +30,7 @@ csdn_support.prototype.add_good_zone = function ($, result) {
     result.push({skilledTopics: skilledTopics});
 };
 
-csdn_support.prototype.get = function(name, callback){
+zhihu_support.prototype.get = function(name, callback){
     'use strict';
     jsdom.env({
         url: "http://www.zhihu.com/people/phodal",
@@ -39,11 +39,9 @@ csdn_support.prototype.get = function(name, callback){
             var $ = window.$;
             var result = [];
 
-            console.log(result);
-
-            csdn_support.prototype.add_user_agree($, result);
-            csdn_support.prototype.add_user_thanks($, result);
-            csdn_support.prototype.add_good_zone($, result);
+            zhihu_support.prototype.add_user_agree($, result);
+            zhihu_support.prototype.add_user_thanks($, result);
+            zhihu_support.prototype.add_good_zone($, result);
 
             callback(result);
         }
@@ -51,4 +49,4 @@ csdn_support.prototype.get = function(name, callback){
 
 };
 
-module.exports = csdn_support;
+module.exports = zhihu_support;
