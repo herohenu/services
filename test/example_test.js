463e4d83f7bd3ea7523bb45c997f9d909e72e318
Fengda HUANG <h@phodal.com>
2014-11-27 09:04:05 +0800

    add the nock libraray

--- test/example_test.js
+++ test/example_test.js
@@ -1,20 +1,42 @@
 var chai            = require("chai");
 var sinon           = require("sinon");
+//var nock            = require('nock');
 var Q               = require("q");
+
 var Zhihu           = require("../server/support/zhihu");
 
 var zhihu = new Zhihu();
 
 describe('Website Support Test', function () {
     before(function () {
-
+        //nock('http://www.zhihu.com/people/phodal')
+        //    .get('/')
+        //    .delayConnection(2000)
+        //    .reply(200, "")
     });
 
-    it('should return zhihu result of phodal', function (done) {
-        var success = sinon.spy();
-        var fail = sinon.spy();
-        var result = [];
+    var initVal = function (result) {
+        'use strict';
+        result = [];
+        return result;
+    };
 
+    var zhihu_get = function(result, name){
+        'use strict';
+        return zhihu.get(result, name);
+    };
+
+
+    it('should return zhihu result of phodal', function (done) {
+        //var success = sinon.spy();
+        //var fail = sinon.spy();
+        //var result = [];
+        //Q.fcall(initVal)
+        //    .then(zhihu_get)
+        //    .then(function(result){
+        //        console.log(result);
+        //        done();
+        //    });
         done();
     });
 });
